753e0d41d21cab1172eb963a252794d4441b66d8
Eligorkos <lzgnom22@gmail.com>
2022-11-19 13:17:37 +0700

    work

---
+++ src/Header/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './Header.css'
+
+function Header(){
+    return (
+        <header className="header"> 
+            <nav>
+                <ul>
+                    jhfgrhgf
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
